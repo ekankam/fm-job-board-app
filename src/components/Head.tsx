f3c02c0efe15ed58598939c701f92b7dceec7725
ekankam <eb.kankam26@gmail.com>
2023-01-20 18:38:51 -0600

    Format files

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 type HeadTagProp = {
   tag: string;
-}
+};
 
 export default function HeadTag({ tag }: HeadTagProp) {
   return (
@@ -10,7 +10,11 @@ export default function HeadTag({ tag }: HeadTagProp) {
       <title>{tag}</title>
       <meta name="description" content="Frontend Mentor Job Board Challenge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="shortcut icon" href="/assets/favicon-32x32.png" type="image/x-icon" />
+      <link
+        rel="shortcut icon"
+        href="/assets/favicon-32x32.png"
+        type="image/x-icon"
+      />
     </Head>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-import HeadTag from '@/components/Head'
-
+import HeadTag from "@/components/Head";
+import Switch from "@/ui/Switch";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <HeadTag tag="Home" />
-      <main className='text-4xl text-yellow-400'>This is Home page</main>
-    </>
-  )
+      <main className="bg-custom-gray-200 dark:bg-dark-blue min-h-screen h-full max-w-[1440px] w-full mx-auto">
+        <Switch />
+      </main>
+    </div>
+  );
 }
