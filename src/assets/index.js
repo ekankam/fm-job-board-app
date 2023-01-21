b354e4281b93b5246901045318b7bd517c1ea038
ekankam <eb.kankam26@gmail.com>
2023-01-20 18:41:43 -0600

    Add webpack to import svgs

---
+++ src/assets/index.js
@@ -0,0 +1,72 @@
+const baseSrc = "/assets";
+const desktop = `${baseSrc}/desktop`;
+const logos = `${baseSrc}/logos`;
+const mobile = `${baseSrc}/mobile`;
+const tablet = `${baseSrc}/tablet`;
+
+// Desktop Svgs
+const backgroundPatternDetailFooter = `${desktop}/bg-pattern-detail-footer.svg`;
+const backgroundPatternHeader = `${desktop}/bg-pattern-header.svg`;
+const checkIcon = `${desktop}/icon-check.svg`;
+const locationIcon = `${desktop}/icon-location.svg`;
+const logoIcon = `${desktop}/logo.svg`;
+const moonIcon = `${desktop}/icon-moon.svg`;
+const searchIcon = `${desktop}/icon-search.svg`;
+const sunIcon = `${desktop}/icon-sun.svg`;
+
+// logos Svgs
+const blogr = `${logos}/blogr.svg`;
+const coffeeRoasters = `${logos}/coffeeroasters.svg`;
+const creative = `${logos}/creative.svg`;
+const crowdFund = `${logos}/crowdfund.svg`;
+const maker = `${logos}/maker.svg`;
+const masterCraft = `${logos}/masterCraft.svg`;
+const officeLite = `${logos}/officelite.svg`;
+const pod = `${logos}/pod.svg`;
+const pomodoro = `${logos}/pomodoro.svg`;
+const scoot = `${logos}/scoot.svg`;
+const typeMaster = `${logos}/typemaster.svg`;
+const vector = `${logos}/vector.svg`;
+
+// Mobile Svgs
+const backgroundPatternDetailFooterMobile = `${mobile}/bg-pattern-detail-footer.svg`;
+const backgroundPatternHeaderMobile = `${mobile}/bg-pattern-header.svg`;
+const filterIcon = `${mobile}/icon-filter.svg`;
+
+// Tablet Svg
+const backgroundPatternHeaderTablet = `${tablet}/bg-pattern-header.svg`;
+
+export const Icons = {
+  desktop: {
+    backgroundPatternDetailFooter,
+    backgroundPatternHeader,
+    checkIcon,
+    locationIcon,
+    moonIcon,
+    searchIcon,
+    sunIcon,
+    logoIcon,
+  },
+  logos: {
+    blogr,
+    coffeeRoasters,
+    creative,
+    crowdFund,
+    maker,
+    masterCraft,
+    officeLite,
+    pod,
+    pomodoro,
+    scoot,
+    typeMaster,
+    vector,
+  },
+  mobile: {
+    backgroundPatternDetailFooterMobile,
+    backgroundPatternHeaderMobile,
+    filterIcon,
+  },
+  tablet: {
+    backgroundPatternHeaderTablet,
+  },
+};
