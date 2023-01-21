b1c942082122767f42031f81c5f7e58e18498171
ekankam <eb.kankam26@gmail.com>
2023-01-20 18:44:21 -0600

    Format files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit",
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontSize: {
-      sm: ['0.875rem', '1.125rem'],
-      base: ['1rem', '1.625rem'],
-      xl: ['1.25rem', '1.5rem'],
-      '2xl': ['1.5rem', '1.8125rem'],
-      '3xl': ['1.75rem', '2.125rem']
-    },
-    colors: {
-      "violet": {
-        "700": "#5964E0",
-        "500": "#939BF4"
+    extend: {
+      fontSize: {
+        14: ["0.875rem", "1.125rem"],
+        16: ["1rem", "1.625rem"],
+        20: ["1.25rem", "1.5rem"],
+        24: ["1.5rem", "1.8125rem"],
+        28: ["1.75rem", "2.125rem"],
       },
-      "dark-blue": "#19202D",
-      "midnight": "#121721",
-      "white": "#FFFFFF",
-      "gray": {
-        "200": "#F4F6F8",
-        "500": "#9DAEC2",
-        "700": "#6E8098"
-      }
-    }
+      colors: {
+        violet: {
+          700: "#5964E0",
+          500: "#939BF4",
+        },
+        "dark-blue": "#19202D",
+        midnight: "#121721",
+        white: "#FFFFFF",
+        "custom-gray": {
+          200: "#F4F6F8",
+          500: "#9DAEC2",
+          700: "#6E8098",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
