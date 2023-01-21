83215870410be0fadc560fac3246c7dcb87b865d
ekankam <eb.kankam26@gmail.com>
2023-01-20 18:42:38 -0600

    Implement dark mode

--- src/ui/Switch.tsx
+++ src/ui/Switch.tsx
@@ -1,21 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 import { motion } from "framer-motion";
 import classNames from "classnames";
+import { Icons } from "@/assets";
+import Image from "next/image";
 
-type SwitchProps = {
-  isClicked: boolean;
-  onClick: () => void;
-}
 const spring = {
   type: "spring",
   stiffness: 500,
-  damping: 30
+  damping: 30,
 };
 
-export default function Switch({ isClicked, onClick }: SwitchProps) {
+export default function Switch(): JSX.Element | null {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  const handleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <div className={classNames("w-12 bg-white rounded-xl p-[5px] cursor-pointer group flex justify-start", { 'justify-end': isClicked })} onClick={onClick}>
-      <motion.div className="bg-violet-700 w-[14px] h-[14px]  rounded-full group-hover:bg-violet-500" layout transition={spring} />
+    <div className="flex items-center gap-4">
+      <Image
+        alt="Sun Icon"
+        src={Icons.desktop.sunIcon}
+        width="20"
+        height="18"
+      />
+      <div
+        className={classNames(
+          "w-12 bg-white rounded-xl p-[5px] cursor-pointer group flex justify-start",
+          { "justify-end": theme === "dark" }
+        )}
+        onClick={handleTheme}
+      >
+        <motion.div
+          className="bg-violet-700 w-[14px] h-[14px]  rounded-full group-hover:bg-violet-500"
+          layout
+          transition={spring}
+        />
+      </div>
+      <Image
+        alt="Moon Icon"
+        src={Icons.desktop.moonIcon}
+        width="12"
+        height="12"
+      />
     </div>
   );
-}     
+}
